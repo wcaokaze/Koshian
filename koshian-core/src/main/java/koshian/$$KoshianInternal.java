994f63194935195b543696d1f61b8e11de16cf80
wcaokaze <wcaokaze@gmail.com>
2020-02-23 03:00:15 +0900

    moved Context into $$KoshianInternal#context

---
+++ koshian-core/src/main/java/koshian/$$KoshianInternal.java
@@ -0,0 +1,9 @@
+package koshian;
+
+import android.annotation.*;
+import android.content.*;
+
+public final class $$KoshianInternal {
+   @SuppressLint("StaticFieldLeak")
+   public static Context context = null;
+}
--- koshian-core/src/main/java/koshian/Koshian.kt
+++ koshian-core/src/main/java/koshian/Koshian.kt
@@ -4,12 +4,19 @@ import android.content.*
 import android.view.*
 
 fun <R> koshian(context: Context, koshianBuilder: Koshian.() -> R): R {
-   val koshian = Koshian(context)
-   return koshian.koshianBuilder()
+   val oldContext = `$$KoshianInternal`.context
+   `$$KoshianInternal`.context = context
+
+   try {
+      val koshian = Koshian()
+      return koshian.koshianBuilder()
+   } finally {
+      `$$KoshianInternal`.context = oldContext
+   }
 }
 
-class Koshian(val context: Context)
+class Koshian
 
 fun Koshian.view(koshianViewBuilder: () -> Unit): View {
-   return View(context)
+   return View(`$$KoshianInternal`.context)
 }
