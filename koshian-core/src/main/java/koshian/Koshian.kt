8c732f64fced7fd83d4b5c792a54ac00e33931dd
wcaokaze <wcaokaze@gmail.com>
2020-02-23 03:22:53 +0900

    made Koshian an inline class

--- koshian-core/src/main/java/koshian/Koshian.kt
+++ koshian-core/src/main/java/koshian/Koshian.kt
@@ -9,20 +9,23 @@ fun <R> koshian(context: Context, koshianBuilder: Koshian<Unit>.() -> R): R {
    `$$KoshianInternal`.context = context
 
    try {
-      val koshian = Koshian(Unit)
+      val koshian = Koshian<Unit>(Unit)
       return koshian.koshianBuilder()
    } finally {
       `$$KoshianInternal`.context = oldContext
    }
 }
 
-class Koshian<out V>(val `$$koshianInternal$view`: V)
+inline class Koshian<out V>(val `$$koshianInternal$view`: Any)
 
-val <V : View> Koshian<V>.view: V get() = `$$koshianInternal$view`
+val <V : View> Koshian<V>.view: V get() {
+   @Suppress("UNCHECKED_CAST")
+   return `$$koshianInternal$view` as V
+}
 
 fun Koshian<*>.textView(koshianViewBuilder: Koshian<TextView>.() -> Unit): TextView {
    val view = TextView(`$$KoshianInternal`.context)
-   val koshian = Koshian(view)
+   val koshian = Koshian<TextView>(view)
    koshian.koshianViewBuilder()
    return view
 }
