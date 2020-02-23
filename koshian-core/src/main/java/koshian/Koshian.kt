0f63ceafbfe7550a73a0e747abba3db22c070ca6
wcaokaze <wcaokaze@gmail.com>
2020-02-24 03:16:59 +0900

    deleted a comment about inline. KoshianViewConstructor is now not inline class

--- koshian-core/src/main/java/koshian/Koshian.kt
+++ koshian-core/src/main/java/koshian/Koshian.kt
@@ -34,42 +34,6 @@ inline class Koshian<out V, out L, out CL>(val `$$koshianInternal$view`: Any?) {
    inline operator fun <C : View>
          KoshianViewConstructor<C>.invoke(koshianViewBuilder: Koshian<C, CL, Nothing>.() -> Unit): C
    {
-      /*
-       * NOTE: This is an inline fun for an inline class,
-       * but the constructor is not inlined.
-       *
-       *     textView {
-       *        view.text = ""
-       *     }
-       *
-       * This will be compiled like the follow:
-       *
-       *     public final class TextViewConstructor implements (Context) -> TextView {
-       *        public static final TextViewConstructor INSTANCE = new TextViewConstructor();
-       *
-       *        @Override
-       *        public TextView invoke(final Context context) {
-       *           return new TextView(context);
-       *        }
-       *     }
-       *
-       *     val provider = TextViewConstructor.INSTANCE
-       *           as (Context) -> TextView // ??? Compiler inserts a cast
-       *
-       *     provider.constructor() // invoked in spite of nop
-       *
-       *     val view = provider(`$$KoshianInternal`.context)
-       *
-       *     val koshian = view
-       *     koshian.constructor()
-       *
-       *     (koshian.`$$koshianInternal$view` as TextView).text = ""
-       *
-       * Not the follow:
-       *
-       *     val view = TextView(`$$KoshianInternal`.context)
-       *     view.text = ""
-       */
       val view = instantiate(`$$KoshianInternal`.context)
 
       val parent = `$$koshianInternal$view` as ViewManager
