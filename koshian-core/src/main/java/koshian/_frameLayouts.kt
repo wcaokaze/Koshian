06d1f20505043a0a308991cf4fc7fadc0dfb74c1
wcaokaze <wcaokaze@gmail.com>
2020-02-24 04:19:35 +0900

    implemented FrameLayoutConstructor

--- koshian-core/src/main/java/koshian/_frameLayouts.kt
+++ koshian-core/src/main/java/koshian/_frameLayouts.kt
@@ -1,5 +1,11 @@
 package koshian
 
+import android.content.*
 import android.widget.*
 
-inline val KoshianParent.frameLayout get() = KoshianViewConstructor(::FrameLayout)
+object FrameLayoutConstructor : KoshianViewGroupConstructor<FrameLayout, FrameLayout.LayoutParams> {
+   override fun instantiate(context: Context) = FrameLayout(context)
+   override fun instantiateLayoutParams() = FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+}
+
+inline val KoshianParent.frameLayout get() = FrameLayoutConstructor
