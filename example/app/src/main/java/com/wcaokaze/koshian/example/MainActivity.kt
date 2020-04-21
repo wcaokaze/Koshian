31f6a2ee46951907ac2d6083757ae8cf4c982447
wcaokaze <wcaokaze@gmail.com>
2020-04-22 02:54:59 +0900

    added some Views

--- example/app/src/main/java/com/wcaokaze/koshian/example/MainActivity.kt
+++ example/app/src/main/java/com/wcaokaze/koshian/example/MainActivity.kt
@@ -17,20 +17,57 @@
 package com.wcaokaze.koshian.example
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import koshian.*
 import kotlin.contracts.ExperimentalContracts
 
 class MainActivity : Activity() {
+   private fun showKoshianGitHubRepository() {
+      val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/wcaokaze/Koshian"))
+      startActivity(intent)
+   }
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
 
       @OptIn(ExperimentalContracts::class)
       val contentView = koshian(this) {
-         FrameLayout {
+         LinearLayout {
+            view.orientation = VERTICAL
+
+            TextView {
+               view.text = "This is an example app for Koshian."
+            }
+
+            TextView {
+               view.text = "https://github.com/wcaokaze/Koshian"
+               view.setOnClickListener { showKoshianGitHubRepository() }
+            }
+         }
+      }
+
+      contentView.applyKoshian(MainActivityStyle()) {
+         view.setPaddingRelative(16.dip, 16.dip, 16.dip, 16.dip)
+
+         TextView {
+         }
+
+         TextView {
+            view.textColor = 0x03a9f4.opaque
          }
       }
 
       setContentView(contentView)
+   }
+
+   class MainActivityStyle : KoshianStyle() {
+      override fun defaultStyle() {
+         TextView {
+            view.setPaddingRelative(8.dip, 8.dip, 8.dip, 8.dip)
+            view.textSizeSp = 16
+         }
+      }
    }
 }
